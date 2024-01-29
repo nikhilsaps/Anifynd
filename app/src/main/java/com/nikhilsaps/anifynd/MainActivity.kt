c21b4cb44b14cfb18d922c94d4612c3aaabc5239
nikhilsaps <nikhilsuccess6@gmail.com>
2024-01-29 12:11:27 +0530

    added  fragments and  transaction with  bottom navigation with UI elements

--- app/src/main/java/com/nikhilsaps/anifynd/MainActivity.kt
+++ app/src/main/java/com/nikhilsaps/anifynd/MainActivity.kt
@@ -2,10 +2,92 @@ package com.nikhilsaps.anifynd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nikhilsaps.anifynd.databinding.ActivityMainBinding
+import com.nikhilsaps.anifynd.fragments.AnimeDBFragment
+import com.nikhilsaps.anifynd.fragments.HomeFragment
+import com.nikhilsaps.anifynd.fragments.ListFragment
+import com.nikhilsaps.anifynd.fragments.ReadDBFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //run this once  to  make  the home fragment appear  on launch
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.mainFragContainer.id,HomeFragment())
+            addToBackStack(null)
+            commit()
+        }
+
+        binding.mainBtmNav.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home_frag ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(binding.mainFragContainer.id,HomeFragment())
+                        addToBackStack(null)
+                        commit()
+                    }
+
+                    true
+                }
+                R.id.list_frag ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(binding.mainFragContainer.id,ListFragment())
+                        addToBackStack(null)
+                        commit()
+                    }
+                    true
+
+                }
+                R.id.anime_frag ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(binding.mainFragContainer.id,AnimeDBFragment())
+                        addToBackStack(null)
+                        commit()
+                    }
+                    true
+                }
+                R.id.read_frag ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(binding.mainFragContainer.id,ReadDBFragment())
+                        addToBackStack(null)
+                        commit()
+                    }
+                    true
+                }
+                else -> false
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.temptext.text= "ni mai ni rok sakta  tujhe "
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
     }
 }
---
+++ app/src/main/java/com/nikhilsaps/anifynd/fragments/HomeFragment.kt
@@ -0,0 +1,61 @@
+package com.nikhilsaps.anifynd.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.nikhilsaps.anifynd.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding?=null
+
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        Toast.makeText(context,"hello to home creating ", Toast.LENGTH_SHORT).show()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(context,"hello to home created  ", Toast.LENGTH_SHORT).show()
+
+        // Now you can access views using binding
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Home", "onRestart: Activity start")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Home", "onRestart: Activity resumed")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Nullify the binding to avoid memory leaks
+        _binding = null
+    }
+}
